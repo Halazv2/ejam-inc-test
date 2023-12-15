1d6b53a7b51f7c9feae6551eb17711349702718e
Halazv2 <halaziani2003@gmail.com>
2023-12-15 12:04:04 +0100

    Add Prettier configuration file and update dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,133 @@
-import typographyPlugin from '@tailwindcss/typography'
-import { type Config } from 'tailwindcss'
-import typographyStyles from './src/util/typography'
+import typographyPlugin from '@tailwindcss/typography';
+import { type Config } from 'tailwindcss';
+
+const rem = (px) => {
+    if (typeof px === 'string') {
+        return Number.parseFloat((Number.parseInt(px) / 16).toString()) + 'rem';
+    } else if (Array.isArray(px)) {
+        return px.map(rem);
+    }
+};
+
+const v = (variableName) => `var(--${variableName})`;
+
+const fontSize = {
+    caption: rem(['12px', '16px']),
+    body: rem(['14px', '20px']),
+    'body-lg': rem(['18px', '24px']),
+    subtitle: rem(['20px', '28px']),
+    'subtitle-lg': rem(['24px', '32px']),
+    title: rem(['28px', '36px']),
+    'title-lg': rem(['36px', '52px']),
+    'title-xl': rem(['52px', '68px']),
+    display: rem(['68px', '92px']),
+};
+
+const textColor = {
+    white: '#ffffff',
+    black: '#000000',
+    primary: v('text-primary'),
+    secondary: v('text-secondary'),
+    tertiary: v('text-tertiary'),
+    disabled: v('text-disabled'),
+    muted: v('text-muted'),
+    danger: v('text-danger'),
+    'muted-dark': v('text-muted-dark'),
+    'validation-error': v('text-validation-error'),
+    'validation-warning': v('text-validation-warning'),
+    'validation-success': v('text-validation-success'),
+    senary: v('bg-solid-senary'),
+    quinary: v('bg-solid-quinary'),
+};
+const backgroundColor = {
+    control: {
+        DEFAULT: v('bg-control-default'),
+    },
+    accent: {
+        DEFAULT: v('bg-accent-default'),
+        disabled: v('bg-accent-disabled'),
+    },
+    solid: {
+        primary: v('bg-solid-primary'),
+        secondary: v('bg-solid-secondary'),
+        tertiary: v('bg-solid-tertiary'),
+        quaternary: v('bg-solid-quaternary'),
+        quinary: v('bg-solid-quinary'),
+    },
+    light: {
+        primary: v('bg-light-primary'),
+        secondary: v('bg-light-secondary'),
+        tertiary: v('bg-light-tertiary'),
+    },
+    subtle: {
+        secondary: v('bg-subtle-secondary'),
+        tertiary: v('bg-subtle-tertiary'),
+    },
+};
+const borderColor = {
+    surface: {
+        DEFAULT: v('border-surface-default'),
+        flyout: v('border-surface-flyout'),
+        tertiary: v('border-surface-tertiary'),
+    },
+    card: {
+        DEFAULT: v('border-card-default'),
+    },
+};
+
+const colors = {
+    system: {
+        attention: '#005fb7',
+        'attention-bg': '#f6f6f6',
+        success: '#0f7b0f',
+        'success-bg': '#dff6dd',
+        caution: '#9d5d00',
+        'caution-bg': '#fff4ce',
+        critical: '#c42b1c',
+        'critical-bg': '#fde7e9',
+    },
+};
+const borderRadius = {
+    DEFAULT: rem('10px'),
+    base: rem('4px'),
+    full: rem('9999px'),
+};
+
+const spacing = {
+    0.75: rem('3px'),
+    1.75: rem('7px'),
+    7.5: rem('30px'),
+};
+
+const boxShadow = {
+    tooltip: v('shadow-tooltip'),
+    flyout: v('shadow-flyout'),
+};
+
+const gridAutoRows = { 17.5: rem('70px') };
+const gridAutoColumns = { 20: rem('80px') };
+
+const transitionTimingFunction = {
+    point: 'cubic-bezier(0.55, 0.55, 0, 1)',
+};
 
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: 'class',
-  plugins: [typographyPlugin],
-  theme: {
-    fontSize: {
-      xs: ['0.8125rem', { lineHeight: '1.5rem' }],
-      sm: ['0.875rem', { lineHeight: '1.5rem' }],
-      base: ['1rem', { lineHeight: '1.75rem' }],
-      lg: ['1.125rem', { lineHeight: '1.75rem' }],
-      xl: ['1.25rem', { lineHeight: '2rem' }],
-      '2xl': ['1.5rem', { lineHeight: '2rem' }],
-      '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
-      '4xl': ['2rem', { lineHeight: '2.5rem' }],
-      '5xl': ['3rem', { lineHeight: '3.5rem' }],
-      '6xl': ['3.75rem', { lineHeight: '1' }],
-      '7xl': ['4.5rem', { lineHeight: '1' }],
-      '8xl': ['6rem', { lineHeight: '1' }],
-      '9xl': ['8rem', { lineHeight: '1' }],
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    darkMode: 'class',
+    plugins: [typographyPlugin],
+    theme: {
+        fontSize,
+        textColor,
+        extend: {
+            spacing,
+            backgroundColor,
+            borderColor,
+            colors,
+            borderRadius,
+            boxShadow,
+            gridAutoRows,
+            gridAutoColumns,
+            transitionTimingFunction,
+        },
     },
-    typography: typographyStyles,
-  },
-} satisfies Config
+} satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@views': path.resolve(__dirname, 'src/views'),
+            '@assets': path.resolve(__dirname, 'src/assets'),
+            '@styles': path.resolve(__dirname, 'src/styles'),
+            '@utils': path.resolve(__dirname, 'src/utils'),
+            '@hooks': path.resolve(__dirname, 'src/hooks'),
+            '@helpers': path.resolve(__dirname, 'src/helpers'),
+            '@layouts': path.resolve(__dirname, 'src/layouts'),
+            '@types': path.resolve(__dirname, 'src/types'),
+        },
+    },
+});
